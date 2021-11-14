d90f053d30cb524829ec11aaa0845243d8d583ba
Romie Greenbourg <informathemusic@gmail.com>
2021-11-14 22:16:23 +0100

    Enable over caching for CDNs

--- sw.js
+++ sw.js
@@ -29,10 +29,10 @@ self.addEventListener('fetch', (e) => {
   e.respondWith((async () => {
     const r = await caches.match(e.request);
     console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-    if (!navigator.onLine) return r;
+    if (!navigator.onLine || !e.request.referrerPolicy === 'no-referrer') return r;
     const response = await fetch(e.request);
     const cache = await caches.open(cacheName);
-    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    console.log(`[Service Worker] Caching resource: ${e.request.url}`);
     cache.put(e.request, response.clone());
     return response;
   })());
