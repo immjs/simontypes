593aa7e44ea89d13feca62b8fdce5d4efeeef935
Romie Greenbourg <informathemusic@gmail.com>
2021-11-14 20:28:44 +0100

    Overreliant on caching, make network requests when possible

--- sw.js
+++ sw.js
@@ -29,7 +29,7 @@ self.addEventListener('fetch', (e) => {
   e.respondWith((async () => {
     const r = await caches.match(e.request);
     console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-    if (r) return r;
+    if (!navigator.onLine) return r;
     const response = await fetch(e.request);
     const cache = await caches.open(cacheName);
     console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
